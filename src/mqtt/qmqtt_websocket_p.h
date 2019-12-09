2d0b7dd96a1c1a75fc833ffc0d34f805e67d9f02
Matthias Dieter Wallnöfer <mwallnoefer@users.noreply.github.com>
2019-12-09 19:28:39 +0100

    Update qmqtt_websocket_p.h
    better include QSslConfiguration as requested by the method QSslConfiguration sslConfiguration() const

--- src/mqtt/qmqtt_websocket_p.h
+++ src/mqtt/qmqtt_websocket_p.h
@@ -45,8 +45,9 @@
 #include <QAbstractSocket>
 
 QT_FORWARD_DECLARE_CLASS(QIODevice)
+
 #ifndef QT_NO_SSL
-QT_FORWARD_DECLARE_CLASS(QSslConfiguration)
+#include <QSslConfiguration>
 QT_FORWARD_DECLARE_CLASS(QSslError)
 #endif // QT_NO_SSL
 
